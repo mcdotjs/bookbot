22a879eccec19f489cc3cd3ea77207061d06eb9c
mcdotjs <mirectm@gmail.com>
2024-05-05 12:30:18 +0200

    word counts

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
-
+    words_count = get_book_word_counts(text)
+    # print(text)
+    print(f"words count is: {words_count}")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_book_word_counts(str):
+    words = str.split()
+    return len(words)
 
 
 main()
